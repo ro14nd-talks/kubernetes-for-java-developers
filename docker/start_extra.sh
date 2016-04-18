507cd109d00dc174edf487acd9460a25afa536be
Roland Huß <roland@ro14nd.de>
2016-04-18 22:07:18 +0200

    Worked on ...

--- docker/start_extra.sh
+++ docker/start_extra.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Starting Visualizer at port 8001 ...."
 cd /visualizer
-kubectl proxy -p 8001 --www=$(pwd) --www-prefix=/k8s --api-prefix=/api/
+kubectl proxy -p 8001 --address=0.0.0.0 --accept-hosts="^.*$" --www=$(pwd) --www-prefix=/k8s --api-prefix=/api/
 
