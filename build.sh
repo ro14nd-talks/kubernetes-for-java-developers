3fc628a0bdf5325bb2702c19b99d7def910e8815
Roland Huß <roland@ro14nd.de>
2016-06-16 10:09:44 +0200

    Update fo jbcnf

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "Building rhuss/talk-kubernetes-2016-jax"
+echo "Building rhuss/talk-kubernetes-2016"
 extra_args=""
 if [ x$1 != x ]; then
   extra_args="--build-arg KUBERNETES_MASTER=$1"
 fi
-docker --tlsverify=false build ${extra_args} -t rhuss/talk-kubernetes-2016-jax .
+docker build ${extra_args} -t rhuss/talk-kubernetes-2016:jbcnf .
--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 echo "Starting presentation"
 
-docker --tlsverify=false run -it --rm \
+docker run -it --rm \
       -v /var/run/docker.sock:/var/run/docker.sock \
-       -v $(pwd)/m2:/root/.m2 \
+      -v $(pwd)/m2:/root/.m2 \
       -v $(pwd)/slides:/slides \
       -v $(pwd)/demo:/demo \
-      -e MAVEN_OPTS="-Ddocker.host.address=172.17.42.1" \
-      -p 9000:9000 -p 57575:57575 -p 35729:35729 -p 8001:8001 \
-      rhuss/talk-kubernetes-2016-jax $*
+      -e MAVEN_OPTS="-Ddocker.host.address=127.0.0.1" \
+      -p 9000:9000 -p 57575:57575 -p 35729:35729 -p 8001:8001 -p 8080:8080 \
+      rhuss/talk-kubernetes-2016:jbcnf $*
 
 #      -v $(pwd)/m2:/root/.m2 \
 
